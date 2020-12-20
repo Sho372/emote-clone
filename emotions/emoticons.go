2d87c2a504242cf86d696cf7284cab393021811d
Sho372 <32non23ca@gmail.com>
2020-12-21 03:11:30 +0900

    add emoticons.go

--- emotions/emoticons.go
+++ emotions/emoticons.go
@@ -1,11 +1,40 @@
 package emotions
 
 import (
-	"github.com/Sho372/emote-clone/config"
+	"fmt"
 	"io"
+	"os"
+
+	"github.com/Sho372/emote-clone/config"
+	"github.com/atotto/clipboard"
 )
 
 type App struct {
 	Out    io.Writer
 	Config *config.Config
+}
+
+func New() (*App, error) {
+	c, err := config.Load()
+	if err != nil {
+		return nil, err
+	}
+	a := &App{
+		Out:    os.Stdout,
+		Config: c,
+	}
+	return a, nil
+}
+
+func (a *App) Emote(name string, dest string) {
+	emoticon := a.Config.Emoticon[name]
+
+	switch dest {
+	case "clipboard":
+		clipboard.WriteAll(emoticon)
+		fmt.Fprintf(a.Out, emoticon, "was copied to the clipboard")
+	default:
+		fmt.Fprintf(a.Out, emoticon)
+	}
+
 }
--- go.mod
+++ go.mod
@@ -3,6 +3,7 @@ module github.com/Sho372/emote-clone
 go 1.15
 
 require (
+	github.com/atotto/clipboard v0.1.2
 	github.com/spf13/pflag v1.0.5 // indirect
 	github.com/spf13/viper v1.7.1
 	gopkg.in/yaml.v2 v2.2.8 // indirect
